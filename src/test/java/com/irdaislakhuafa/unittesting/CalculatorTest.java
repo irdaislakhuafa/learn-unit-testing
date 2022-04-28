72cc0ca2599abfce5e72fe24a2ca5091cdd0a665
irda islakhu afa <irdhaislakhuafa@gmail.com>
2022-04-28 10:01:19 -0700

    modify something

--- src/test/java/com/irdaislakhuafa/unittesting/CalculatorTest.java
+++ src/test/java/com/irdaislakhuafa/unittesting/CalculatorTest.java
@@ -1,12 +1,16 @@
 package com.irdaislakhuafa.unittesting;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Test for calculator class")
 public class CalculatorTest {
     private final Calculator calculator = new Calculator();
 
     @Test
+    @DisplayName("add success test")
     public void testAddSuccess() {
         var result = calculator.add(1, 2);
         assertNotNull(calculator);
@@ -16,6 +20,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("divide success test")
     public void testDivideSuccess() {
         var result = calculator.divide(40, 2);
         assertNotNull(result);
